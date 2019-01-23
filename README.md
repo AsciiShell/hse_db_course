ac139aa0779de915293fdc7607757de45aaea6ce
asciishell <asciishell@yandex.ru>
2019-01-24 01:19:27 +0300

    Create code

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # DB_course
 Labs and homeworks for DB course
+
+Useful tips
+
+```mysql
+-- Show errors
+SHOW ENGINE INNODB STATUS
+
+```
--- createStm.sql
+++ createStm.sql
@@ -1,8 +1,4 @@
-CREATE DATABASE IF NOT EXISTS fitness;
-
-USE fitness;
-
-CREATE TABLE IF NOT EXISTS `group`
+CREATE TABLE IF NOT EXISTS `team`
 (
   name VARCHAR(20) PRIMARY KEY,
   note VARCHAR(500) NOT NULL
@@ -12,31 +8,41 @@ CREATE TABLE IF NOT EXISTS `client`
 (
   fio         VARCHAR(50)      NOT NULL,
   ticket      INTEGER PRIMARY KEY AUTO_INCREMENT,
-  group_name  VARCHAR(20)      NOT NULL REFERENCES `group` (name) ON DELETE SET NULL,
+  team_name   VARCHAR(20)      NOT NULL,
   birthday    DATE             NOT NULL,
-  gender      CHAR             NOT NULL CHECK ( gender IN ('m', 'f') ),
+  gender      CHAR             NOT NULL,
   weigh       INTEGER UNSIGNED NOT NULL,
   startTicker DATE             NOT NULL,
-  endTicket   DATE             NOT NULL CHECK ( endTicket >= startTicker ),
-  phone       CHAR(11)         NOT NULL CHECK ( phone NOT LIKE '%[^0-9]%' )
+  endTicket   DATE             NOT NULL,
+  phone       CHAR(11)         NOT NULL,
+  CONSTRAINT `fk_team_client` FOREIGN KEY (team_name) REFERENCES `team` (name) ON DELETE RESTRICT,
+  CONSTRAINT `ch_gender` CHECK (gender IN ('m', 'f')),
+  CONSTRAINT `ch_ticket` CHECK (endTicket >= startTicker),
+  CONSTRAINT `ch_phone_client` CHECK (phone NOT LIKE '%[^0-9]%')
 ) ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS `coach`
 (
-  id     INTEGER(5) PRIMARY KEY AUTO_INCREMENT,
+  id     NUMERIC(5) PRIMARY KEY,
   fio    VARCHAR(50) NOT NULL,
   status VARCHAR(50) NOT NULL,
-  phone  CHAR(11)    NOT NULL CHECK ( phone NOT LIKE '%[^0-9]%' )
+  phone  CHAR(11)    NOT NULL,
+  CONSTRAINT `ch_phone_coach` CHECK ( phone NOT LIKE '%[^0-9]%' )
 ) ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS `scheduler`
 (
   id          NUMERIC(6) PRIMARY KEY,
-  group_name  VARCHAR(20) REFERENCES `group` (name) ON DELETE CASCADE,
+  team_name   VARCHAR(20) NOT NULL,
   coach_id    NUMERIC(5)  NOT NULL,
   kind        VARCHAR(30) NOT NULL,
-  room        VARCHAR(10) NOT NULL CHECK ( room IN ('Бассейн', 'Беговой', 'Силовой', 'Альминизм')),
-  day_of_week CHAR(2)     NOT NULL CHECK ( day_of_week IN ('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс')),
+  room        VARCHAR(10) NOT NULL,
+  day_of_week CHAR(2)     NOT NULL,
   start       TIME        NOT NULL,
-  end         TIME        NOT NULL CHECK ( end > start )
+  end         TIME        NOT NULL,
+  CONSTRAINT `fk_team_scheduler` FOREIGN KEY (team_name) REFERENCES `team` (name) ON DELETE RESTRICT,
+  CONSTRAINT `fk_coach_scheduler` FOREIGN KEY (coach_id) REFERENCES `coach` (id) ON DELETE RESTRICT,
+  CONSTRAINT `ch_room` CHECK ( room IN ('Бассейн', 'Беговой', 'Силовой', 'Альпинизм')),
+  CONSTRAINT `ch_day` CHECK ( day_of_week IN ('пн', 'вт', 'ср', 'чт', 'пт', 'сб', 'вс')),
+  CONSTRAINT `ch_time` CHECK ( end > start )
 ) ENGINE = INNODB;
