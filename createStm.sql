1ebe8c819037359e38cc3b22bf60c75718379edc
asciishell <asciishell@yandex.ru>
2019-04-29 10:28:02 +0300

    FIx structure

--- createStm.sql
+++ createStm.sql
@@ -1,133 +1,45 @@
-CREATE DATABASE IF NOT EXISTS fitness;
-
-USE fitness;
-
-CREATE TABLE IF NOT EXISTS `team`
+CREATE TABLE team
 (
-  name VARCHAR(40) PRIMARY KEY,
-  note VARCHAR(500) NOT NULL
-) ENGINE = INNODB;
+    name VARCHAR(40) PRIMARY KEY,
+    note VARCHAR(500) NOT NULL
+);
 
-CREATE TABLE IF NOT EXISTS `client`
+CREATE TABLE clients
 (
-  fio         VARCHAR(50)      NOT NULL,
-  ticket      INTEGER PRIMARY KEY AUTO_INCREMENT,
-  team_name   VARCHAR(40)      NOT NULL,
-  birthday    DATE             NOT NULL,
-  gender      ENUM ('m', 'f')  NOT NULL,
-  weigh       INTEGER UNSIGNED NOT NULL,
-  startTicker DATE             NOT NULL,
-  endTicket   DATE             NOT NULL,
-  phone       CHAR(11)         NOT NULL,
-  CONSTRAINT `fk_team_client` FOREIGN KEY (team_name) REFERENCES `team` (name) ON DELETE RESTRICT,
-  CONSTRAINT `ch_ticket` CHECK (endTicket >= startTicker)
-) ENGINE = INNODB;
+    fio         VARCHAR(100) NOT NULL,
+    ticket      INTEGER PRIMARY KEY,
+    team_name   VARCHAR(40)  NOT NULL,
+    birthday    DATE         NOT NULL,
+    gender      CHAR(1)      NOT NULL CHECK ( gender IN ('m', 'f') ),
+    weigh       INTEGER      NOT NULL CHECK ( weigh > 0 ),
+    startTicker DATE         NOT NULL,
+    endTicket   DATE         NOT NULL,
+    phone       CHAR(11)     NOT NULL,
+    CONSTRAINT fk_team_client FOREIGN KEY (team_name) REFERENCES team (name) ON DELETE CASCADE,
+    CONSTRAINT ch_ticket CHECK (endTicket >= startTicker)
+);
 
-DELIMITER $$
 
-CREATE PROCEDURE `check_client`(IN startTime DATE, IN endTime DATE)
-BEGIN
-  IF startTime > endTime THEN
-    SIGNAL SQLSTATE '45001'
-      SET MESSAGE_TEXT = 'check constraint on client.time failed';
-  END IF;
-END$$
-
-CREATE PROCEDURE `check_phone`(IN phone CHAR(11))
-BEGIN
-  IF REGEXP_LIKE(phone, '[^0-9]') THEN
-    SIGNAL SQLSTATE '45002'
-      SET MESSAGE_TEXT = 'check constraint on client.phone failed';
-  END IF;
-END $$
-
-CREATE TRIGGER `client_before_insert`
-  BEFORE INSERT
-  ON `client`
-  FOR EACH ROW
-BEGIN
-  CALL check_client(new.startTicker, new.endTicket);
-  CALL check_phone(new.phone);
-END$$
-
-CREATE TRIGGER `client_before_update`
-  BEFORE UPDATE
-  ON `client`
-  FOR EACH ROW
-BEGIN
-  CALL check_client(new.startTicker, new.endTicket);
-  CALL check_phone(new.phone);
-END$$
-
-DELIMITER ;
-
-CREATE TABLE IF NOT EXISTS `coach`
+CREATE TABLE coach
 (
-  id     NUMERIC(5) PRIMARY KEY,
-  fio    VARCHAR(50) NOT NULL,
-  status VARCHAR(50) NOT NULL,
-  phone  CHAR(11)    NOT NULL
-) ENGINE = INNODB;
+    id     NUMERIC(5) PRIMARY KEY,
+    fio    VARCHAR(100) NOT NULL,
+    status VARCHAR(50)  NOT NULL,
+    phone  CHAR(11)     NOT NULL
+);
 
-DELIMITER $$
-
-CREATE TRIGGER `coach_before_insert`
-  BEFORE INSERT
-  ON `coach`
-  FOR EACH ROW
-BEGIN
-  CALL check_phone(new.phone);
-END$$
-
-CREATE TRIGGER `coach_before_update`
-  BEFORE UPDATE
-  ON `coach`
-  FOR EACH ROW
-BEGIN
-  CALL check_phone(new.phone);
-END$$
-
-CREATE TABLE IF NOT EXISTS `scheduler`
+CREATE TABLE schedulers
 (
-  id          NUMERIC(6) PRIMARY KEY,
-  team_name   VARCHAR(40)                                         NOT NULL,
-  coach_id    NUMERIC(5)                                          NOT NULL,
-  kind        VARCHAR(30)                                         NOT NULL,
-  room        ENUM ('Бассейн', 'Беговой', 'Силовой', 'Альпинизм') NOT NULL,
-  day_of_week ENUM ('вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб')     NOT NULL,
-  start       TIME                                                NOT NULL,
-  end         TIME                                                NOT NULL,
-  CONSTRAINT `fk_team_scheduler` FOREIGN KEY (team_name) REFERENCES `team` (name) ON DELETE RESTRICT,
-  CONSTRAINT `fk_coach_scheduler` FOREIGN KEY (coach_id) REFERENCES `coach` (id) ON DELETE RESTRICT,
-  CONSTRAINT `ch_time` CHECK ( end > start )
-) ENGINE = INNODB;
-
-DELIMITER $$
-
-CREATE PROCEDURE `check_scheduler`(IN startTime TIME, IN endTime TIME)
-BEGIN
-  IF endTime <= startTime THEN
-    SIGNAL SQLSTATE '45003'
-      SET MESSAGE_TEXT = 'check constraint on scheduler.time failed';
-  END IF;
-END $$
-
-CREATE TRIGGER `scheduler_before_insert`
-  BEFORE INSERT
-  ON `scheduler`
-  FOR EACH ROW
-BEGIN
-  CALL check_scheduler(new.start, new.end);
-END$$
-
-CREATE TRIGGER `scheduler_before_update`
-  BEFORE UPDATE
-  ON `scheduler`
-  FOR EACH ROW
-BEGIN
-  CALL check_scheduler(new.start, new.end);
-END$$
-
-DELIMITER ;
-
+    id          NUMERIC(6) PRIMARY KEY,
+    team_name   VARCHAR(40) NOT NULL,
+    coach_id    NUMERIC(5)  NOT NULL,
+    kind        VARCHAR(30) NOT NULL,
+    room        CHAR(20)    NOT NULL CHECK (room IN ('Бассейн', 'Беговой', 'Силовой', 'Альпинизм')),
+    day_of_week CHAR(4)     NOT NULL CHECK ( day_of_week IN ('вс', 'пн', 'вт', 'ср', 'чт', 'пт', 'сб') ),
+    start_at    DATE        NOT NULL,
+    end_at      DATE        NOT NULL,
+    CONSTRAINT fk_team_scheduler FOREIGN KEY (team_name) REFERENCES team (name) ON DELETE CASCADE,
+    CONSTRAINT fk_coach_scheduler FOREIGN KEY (coach_id) REFERENCES coach (id) ON DELETE CASCADE,
+    CONSTRAINT ch_DATE CHECK ( end_at > start_at)
+);
 
--- insertScheduler.sql
+++ insertScheduler.sql
@@ -1,21 +1,41 @@
-INSERT INTO `scheduler`
-VALUES (1, 'Chess', 4, 'Тренировка', 'Бассейн', 'пн', '20:01', '20:17'),
-       (2, 'Jumping', 14, 'Экскурсия', 'Бассейн', 'сб', '16:13', '16:40'),
-       (3, 'Jumping', 2, 'Интенсив', 'Альпинизм', 'сб', '14:49', '15:23'),
-       (4, 'Sync swimming', 12, 'Разминка', 'Альпинизм', 'вс', '13:54', '14:04'),
-       (5, 'Sync swimming', 4, 'Экскурсия', 'Силовой', 'сб', '18:24', '18:26'),
-       (6, 'Jumping', 13, 'Интенсив', 'Силовой', 'пн', '12:45', '13:20'),
-       (7, 'Sport swimming', 20, 'Разминка', 'Альпинизм', 'сб', '16:07', '16:31'),
-       (8, 'Sport swimming', 16, 'Теория', 'Беговой', 'сб', '13:34', '14:14'),
-       (9, 'Sport swimming', 7, 'Интенсив', 'Бассейн', 'ср', '19:59', '20:32'),
-       (10, 'Break-dance', 4, 'Практика', 'Альпинизм', 'вт', '11:06', '12:04'),
-       (11, 'Jumping', 16, 'Практика', 'Бассейн', 'вт', '11:04', '11:16'),
-       (12, 'Teacher swimming', 4, 'Тренировка', 'Силовой', 'пн', '12:03', '12:18'),
-       (13, 'Sport swimming', 3, 'Теория', 'Бассейн', 'сб', '17:43', '18:39'),
-       (14, 'Teacher swimming', 20, 'Тренировка', 'Силовой', 'ср', '13:26', '14:17'),
-       (15, 'Running', 1, 'Практика', 'Бассейн', 'сб', '13:50', '14:37'),
-       (16, 'Running', 15, 'Практика', 'Беговой', 'вс', '16:59', '17:51'),
-       (17, 'Fox-hunting', 10, 'Интенсив', 'Беговой', 'вс', '16:05', '17:01'),
-       (18, 'Break-dance', 10, 'Теория', 'Альпинизм', 'чт', '13:36', '14:02'),
-       (19, 'Sync swimming', 11, 'Практика', 'Силовой', 'пн', '11:49', '12:29'),
-       (20, 'Jumping', 20, 'Интенсив', 'Бассейн', 'чт', '15:55', '16:12');
+ALTER SESSION SET NLS_DATE_FORMAT = 'HH24:MI';
+INSERT INTO schedulers
+VALUES (1, 'Chess', 4, 'Тренировка', 'Бассейн', 'пн', '20:01', '20:17');
+INSERT INTO schedulers
+VALUES (2, 'Jumping', 14, 'Экскурсия', 'Бассейн', 'сб', '16:13', '16:40');
+INSERT INTO schedulers
+VALUES (3, 'Jumping', 2, 'Интенсив', 'Альпинизм', 'сб', '14:49', '15:23');
+INSERT INTO schedulers
+VALUES (4, 'Sync swimming', 12, 'Разминка', 'Альпинизм', 'вс', '13:54', '14:04');
+INSERT INTO schedulers
+VALUES (5, 'Sync swimming', 4, 'Экскурсия', 'Силовой', 'сб', '18:24', '18:26');
+INSERT INTO schedulers
+VALUES (6, 'Jumping', 13, 'Интенсив', 'Силовой', 'пн', '12:45', '13:20');
+INSERT INTO schedulers
+VALUES (7, 'Sport swimming', 20, 'Разминка', 'Альпинизм', 'сб', '16:07', '16:31');
+INSERT INTO schedulers
+VALUES (8, 'Sport swimming', 16, 'Теория', 'Беговой', 'сб', '13:34', '14:14');
+INSERT INTO schedulers
+VALUES (9, 'Sport swimming', 7, 'Интенсив', 'Бассейн', 'ср', '19:59', '20:32');
+INSERT INTO schedulers
+VALUES (10, 'Break-dance', 4, 'Практика', 'Альпинизм', 'вт', '11:06', '12:04');
+INSERT INTO schedulers
+VALUES (11, 'Jumping', 16, 'Практика', 'Бассейн', 'вт', '11:04', '11:16');
+INSERT INTO schedulers
+VALUES (12, 'Teacher swimming', 4, 'Тренировка', 'Силовой', 'пн', '12:03', '12:18');
+INSERT INTO schedulers
+VALUES (13, 'Sport swimming', 3, 'Теория', 'Бассейн', 'сб', '17:43', '18:39');
+INSERT INTO schedulers
+VALUES (14, 'Teacher swimming', 20, 'Тренировка', 'Силовой', 'ср', '13:26', '14:17');
+INSERT INTO schedulers
+VALUES (15, 'Running', 1, 'Практика', 'Бассейн', 'сб', '13:50', '14:37');
+INSERT INTO schedulers
+VALUES (16, 'Running', 15, 'Практика', 'Беговой', 'вс', '16:59', '17:51');
+INSERT INTO schedulers
+VALUES (17, 'Fox-hunting', 10, 'Интенсив', 'Беговой', 'вс', '16:05', '17:01');
+INSERT INTO schedulers
+VALUES (18, 'Break-dance', 10, 'Теория', 'Альпинизм', 'чт', '13:36', '14:02');
+INSERT INTO schedulers
+VALUES (19, 'Sync swimming', 11, 'Практика', 'Силовой', 'пн', '11:49', '12:29');
+INSERT INTO schedulers
+VALUES (20, 'Jumping', 20, 'Интенсив', 'Бассейн', 'чт', '15:55', '16:12');
